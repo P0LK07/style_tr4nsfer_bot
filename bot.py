5bf82562cf316cbe2d36de73f485adff2ab777cb
Front3nd_b0y <hackme00@list.ru>
2021-09-23 22:37:51 +0300

    Heroku connectin part 2

--- bot.py
+++ bot.py
@@ -1,1 +1,50 @@
+"""
+Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
+Deployed using heroku.
+Author: liuhh02 https://medium.com/@liuhh02
+"""
 
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+PORT = int(os.environ.get('PORT', 5000))
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+a = open("token.txt","r").readlines()
+logger = logging.getLogger(__name__)
+TOKEN = a[0]
+a.close()
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    update.message.reply_text('Hi!')
+
+def help(update, context):
+    update.message.reply_text('Help!')
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    dp.add_error_handler(error)
+
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path=TOKEN)
+    updater.bot.setWebhook('https://style-tr4nsfer-bot.herokuapp.com/' + TOKEN)
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
