d364b12f5ebb3a12edaa92e14f4aac03b09c51fd
P0LK07 <55788168+P0LK07@users.noreply.github.com>
2021-10-12 19:25:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,3 @@
-"""
-Simple Bot to reply to Telegram messages taken from the python-telegram-bot examples.
-Deployed using heroku.
-Author: liuhh02 https://medium.com/@liuhh02
-"""
-
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
