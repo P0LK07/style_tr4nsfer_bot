72e7bd92b843257485ccf4a8c8c6b438e05b5e9e
P0LK07 <55788168+P0LK07@users.noreply.github.com>
2021-10-20 14:20:44 +0300

    Kek

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def main():
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
 
-    dp.add_handler(MessageHandler(Filters.image, reply))
+    dp.add_handler(MessageHandler(Filters.document.image, reply))
 
     dp.add_error_handler(error)
 
